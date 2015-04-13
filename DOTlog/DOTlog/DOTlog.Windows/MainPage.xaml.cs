e6a45a0f4b85fb4e5a077e1cb82789bd04cba83c
thatdenalidude <thatdenalidude@gmail.com>
2015-04-12 16:36:55 -0800

    added files

---
+++ DOTlog/DOTlog/DOTlog.Windows/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Runtime.Serialization.Json;
+using Windows.Globalization;
+using Windows.Globalization.DateTimeFormatting;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DOTlog
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MemoryStream memoryStream = new MemoryStream();
+        DataContractJsonSerializer serialer = new DataContractJsonSerializer(typeof(jsonClass));
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(EditEvents));
+        }
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SyncEvents));
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string dateAndTimeOfEvent;
+
+            DateTimeFormatter dateFormatter = new DateTimeFormatter("shortdate");
+            DateTimeFormatter timeFormatter = new DateTimeFormatter("shorttime");
+
+            // We use a calendar to determine daylight savings time transition days
+            Calendar calendar = new Calendar();
+            calendar.ChangeClock("24HourClock");
+
+            // The value of the selected time in a TimePicker is stored as a TimeSpan, so it is possible to add it directly to the value of the selected date
+            DateTimeOffset selectedDate = this.dateField.Date;
+            DateTimeOffset combinedValue = new DateTimeOffset(new DateTime(selectedDate.Year, selectedDate.Month, selectedDate.Day) + this.timeField.Time);
+
+            calendar.SetDateTime(combinedValue);
+
+            dateAndTimeOfEvent = dateFormatter.Format(combinedValue) + " " + timeFormatter.Format(combinedValue);
+            
+            memoryStream = new MemoryStream();
+            jsonClass eventObj = new jsonClass((string)airportField.SelectedItem, (string)categoryField.SelectedItem, dateAndTimeOfEvent, eventDescriptionField.Text, (bool)includeInReportField.IsChecked);
+            serialer.WriteObject(memoryStream, eventObj);
+            byte[] jsonArray = memoryStream.ToArray();
+            memoryStream.Dispose();
+            jsonResult.Text = Encoding.UTF8.GetString(jsonArray, 0, jsonArray.Length);
+        }
+    }
+}
---
+++ DOTlog/DOTlog/DOTlog.Windows/jsonClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTlog
+{
+    [DataContract]
+    public class jsonClass
+    {
+        [DataMember]
+        public string airport
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string category
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string timeOfEvent
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string eventDescription
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public bool includeInReport
+        {
+            get;
+            set;
+        }
+
+        public jsonClass()
+        {
+
+        }
+
+        public jsonClass(   string Airport, 
+                            string Category, 
+                            string TimeOfEvent, 
+                            string EventDescription,
+                            bool IncludeInReport)
+        {
+            airport = Airport;
+            category = Category;
+            timeOfEvent = TimeOfEvent;
+            eventDescription = EventDescription;
+            includeInReport = IncludeInReport;
+        }
+    }
+}
